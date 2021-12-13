57c3d15bb60e3af14d453dfb2663f3279a583d41
SJHaubi <88908284+SJHaubi@users.noreply.github.com>
2021-12-13 15:43:47 +0100

    added Guestmode
    added Guestmode

--- App/app/src/main/java/de/klassensuchter/kstippspiel/Main_menu.kt
+++ App/app/src/main/java/de/klassensuchter/kstippspiel/Main_menu.kt
@@ -1,25 +1,42 @@
 package de.klassensuchter.kstippspiel
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 class Main_menu : AppCompatActivity() {
+
+    private lateinit var shared: SharedPreferences
+    private var UserLoggedInAsGuest = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
 
-
+        shared = this.getSharedPreferences("SharedPreferences", MODE_PRIVATE)
 
 
     }
 
     fun buttonSignIn(view: View) {
-        TODO("opens homescreen if user has accdata saved, else loginscreen")
+
+        if(false){
+            startActivity(Intent(this, Homescreen::class.java))
+            finish()
+
+        } else {
+            TODO("API-request --> does this user exist")
+        }
+
     }
 
     fun buttonSignInAsGuest(view: View) {
-        TODO("opens homescreen as Guest, therefore only to view current standings etc")
+        startActivity(Intent(this, StandingsBundesliga::class.java))
+        finish()
+        UserLoggedInAsGuest = shared.getBoolean("UserLoggedInAsGuest",false)
+        shared.edit().putBoolean("UserLoggedInAsGuest", true).apply()
     }
 
 
--- App/app/src/main/java/de/klassensuchter/kstippspiel/StandingsBundesliga.kt
+++ App/app/src/main/java/de/klassensuchter/kstippspiel/StandingsBundesliga.kt
@@ -1,11 +1,20 @@
 package de.klassensuchter.kstippspiel
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class StandingsBundesliga : AppCompatActivity() {
+
+    private lateinit var shared: SharedPreferences
+    private var UserLoggedInAsGuest = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.standings_bundesliga)
+
+        shared = this.getSharedPreferences("SharedPreferences", MODE_PRIVATE)
+        UserLoggedInAsGuest = shared.getBoolean("UserLoggedInAsGuest",false)
+        shared.edit().putBoolean("UserLoggedInAsGuest", true).apply()
     }
 }
